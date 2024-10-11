d194dbb7d5011b8a5990ed7dc11163003289e5f4
CianDickerHughes <g00415413@atu.ie>
2024-10-11 12:53:22 +0000

    added Lab2

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,6 @@
+// The Footer
+const Footer = () => {
+    return <h3>My Footer in another component</h3>;
+  };
+  
+  export default Footer;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,6 @@
+// The Header
+const Header = () => {
+  return <h1>My Header in another component</h1>;
+};
+
+export default Header;
